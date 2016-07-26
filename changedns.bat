3818a9b32b3600594a6f54bca8af73f895b6ced2
elakamarcus <n0m4d@hotmail.com>
2016-07-26 16:43:22 +0800

    adjust error message and stdout/err

--- changedns.bat
+++ changedns.bat
@@ -11,7 +11,7 @@ SET INT = "%~3"
 GOTO RUN
 
 :ADD
-netsh int ipv4 add dnsserver "%INT%" address=%1 index=1
+netsh int ipv4 add dnsserver "%INT%" address=%1 index=1 1>NUL 2>&1 
 if %errorlevel% EQU 0 (@echo added %1 as primary dns & GOTO END) else ( @echo error & GOTO ERROR)
 
 :REMOVE
@@ -23,7 +23,7 @@ IF "%2"=="add" GOTO ADD
 IF "%2"=="del" GOTO REMOVE
 
 :ERROR
-@echo "Error in parameter syntax. Try again. %0 DNSIP add/del interfacename"
+@echo "Error in parameter syntax. Try again. %0 DNSIP [add|del] interfacename"
 GOTO END
 
 :END
